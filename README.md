7c9630cf1ffb97368ccda853b86172c961748f12
vibezcodr <rachaeleling@gmail.com>
2026-02-03 17:33:40 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+
+  # Valentine's Day Website
+
+  This is a code bundle for Valentine's Day Website. The original project is available at https://www.figma.com/design/6cEQkCLZVNlszakTeGUxU2/Valentine-s-Day-Website.
+
+  ## Running the code
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ src/imports/HeartCoral.tsx
@@ -0,0 +1,11 @@
+import svgPaths from "./svg-yds9ar4x4m";
+
+export default function HeartCoral() {
+  return (
+    <div className="relative size-full" data-name="HeartCoral">
+      <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 56.9157 55.5928">
+        <path d={svgPaths.p129fcc80} fill="var(--fill-0, #EF707A)" id="Heart" />
+      </svg>
+    </div>
+  );
+}
